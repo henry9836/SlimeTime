bfc3dcfcc3425497e17833b2da465b426fea72d7
Henry <henry983615@gmail.com>
2019-09-25 04:40:03 +1200

    Starting to make the scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PlayerController : MonoBehaviour
+{
+    public enum PLAYER
+    {
+        UNASSIGNED,
+        PLAYER1,
+        PLAYER2,
+        PLAYER3,
+        PLAYER4
+    }
+
+    public PLAYER playerType = PLAYER.UNASSIGNED;
+
+    public float speed = 10.0f;
+    public float maxSpeed = 30.0f;
+    public float fireForce = 1.0f;
+    public float fireCoolDown = 0.2f;
+    public float aimDistance = 1.0f;
+
+    void Start()
+    {
+        
+    }
+
+}
