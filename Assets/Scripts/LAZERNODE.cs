3e67bb8de4184d1d35745c2d43956529cc28112a
Vaughan Webb <vaughanw2012@gmail.com>
2019-09-27 22:32:30 +1200

    spawner death ray implmented
    ya yeet

--- Assets/Scripts/LAZERNODE.cs
+++ Assets/Scripts/LAZERNODE.cs
@@ -4,18 +4,103 @@ using UnityEngine;
 
 public class LAZERNODE : MonoBehaviour
 {
-    public struct ScanInfo {
-        public Vector3 postion;
-        public string collisionTag;
-    };
-    public ScanInfo Scan()
+    public List<Vector3> ValidPositionFIRE = new List<Vector3>();
+    public List<Vector3> ValidPositionICE = new List<Vector3>();
+    public List<Vector3> ValidPositionNORMAL = new List<Vector3>();
+    public List<Vector3> InvalidPosition = new List<Vector3>();
+
+    public Vector2 startXYpos;
+    public Vector2 finXYpos;
+    public Vector2 LAZERNODEdetail;
+
+
+    public void Scan()
     {
-        ScanInfo result = new ScanInfo();
-
-        result.postion = new Vector3(0.0f,0.0f,0.0f);
-        result.collisionTag = "Unknown";
+        StartCoroutine(SCAN());
+    }
 
 
-        return (result);
+    public void bucketfull()
+    {
+        for (int i = 0; i < ValidPositionFIRE.Count; i++)
+        {
+            transform.parent.gameObject.GetComponent<slimeSpawner>().ValidPositionsFIRE.Add(ValidPositionFIRE[i]);
+        }
+        for (int i = 0; i < ValidPositionICE.Count; i++)
+        {
+            transform.parent.gameObject.GetComponent<slimeSpawner>().ValidPositionsICE.Add(ValidPositionICE[i]);
+        }
+        for (int i = 0; i < ValidPositionNORMAL.Count; i++)
+        {
+            transform.parent.gameObject.GetComponent<slimeSpawner>().ValidPositionsNORMAL.Add(ValidPositionNORMAL[i]);
+        }
+        for (int i = 0; i < InvalidPosition.Count; i++)
+        {
+            transform.parent.gameObject.GetComponent<slimeSpawner>().InvalidPositions.Add(InvalidPosition[i]);
+        }
+
+        Destroy(this.gameObject);
+
+    }
+
+    IEnumerator SCAN()
+    {
+
+        for (float i = startXYpos.x; i < finXYpos.x; i += LAZERNODEdetail.x)
+        {
+
+            RaycastHit hit;
+
+            Vector3[] points = new Vector3[2];
+            points[0] = gameObject.transform.position;
+            points[1] = new Vector3(gameObject.transform.position.x, -100.0f, gameObject.transform.position.z);
+
+
+            GetComponent<LineRenderer>().SetPositions(points);
+            GetComponent<LineRenderer>().enabled = true;
+            if (Physics.Raycast(transform.position, -transform.up, out hit, Mathf.Infinity))
+            {
+
+                if (hit.collider.tag == "FIRE")
+                {
+
+                    ValidPositionFIRE.Add(hit.transform.position);
+                }
+                else if (hit.collider.tag == "ICE")
+                {
+
+                    ValidPositionICE.Add(hit.transform.position);
+                }
+                else if (hit.collider.tag == "NORMAL")
+                {
+
+                    ValidPositionNORMAL.Add(hit.transform.position);
+                }
+                else
+                {
+
+                    InvalidPosition.Add(new Vector3(gameObject.transform.localPosition.x, 0.0f, gameObject.transform.localPosition.z));
+                }
+
+            }
+            else
+            {
+                InvalidPosition.Add(new Vector3(this.gameObject.transform.position.x, 0.0f, this.gameObject.transform.position.z));
+            }
+            //GetComponent<LineRenderer>().enabled = false;
+
+            yield return new WaitForSeconds(0.01f);
+
+            transform.localPosition += new Vector3(LAZERNODEdetail.x, 0.0f, 0.0f);
+
+
+        }
+
+        bucketfull();
+
+        yield return null;
+
+
+
     }
 }
--- Assets/Scripts/slimeSpawner.cs
+++ Assets/Scripts/slimeSpawner.cs
@@ -14,14 +14,16 @@ public class slimeSpawner : MonoBehaviour
     public bool once = true;
     public GameObject LAZERNODEprefab;
     public float LAZERNODEheight;
-    public Vector2Int LAZERNODEdetail;
-    public Vector2Int startXYpos;
-    public Vector2Int finishXYpos;
+    public Vector2 LAZERNODEdetail;
+    public Vector2 startXYpos;
+    public Vector2 finishXYpos;
 
 
-    public Vector3 ValidPositionFIRE;
-    public Vector3 ValidPositionICE;
-    public Vector3 ValidPositionNORMAL;
+    public List<Vector3> ValidPositionsFIRE;
+    public List<Vector3> ValidPositionsICE;
+    public List<Vector3> ValidPositionsNORMAL;
+    public List<Vector3> InvalidPositions;
+
 
 
 
@@ -34,68 +36,75 @@ public class slimeSpawner : MonoBehaviour
             StartCoroutine(LAZERscan());
         }
 
+        
 
-        if (timer >= 2.0f && currentSlimes < maxSlimes)
+        int pointcount = Mathf.FloorToInt((finishXYpos.y - startXYpos.y) / LAZERNODEdetail.y) * Mathf.FloorToInt((finishXYpos.x - startXYpos.x) / LAZERNODEdetail.x);
+
+
+
+        if (ValidPositionsFIRE.Count + ValidPositionsICE.Count + ValidPositionsNORMAL.Count + InvalidPositions.Count == pointcount)
         {
-            currentSlimes += 1;
-
-
-            float rand = Random.Range(0.0f, 100.0f);
-            int HPChosen = 0;
-
-            for (int i = 0; i < slimeHP.Count; i++)
-            {
-                float temp = persentgaes[i];
-
-                for (int j = 0; j < i; j++)
-                {
-                    temp += persentgaes[j];
-                }
-                if (rand < temp)
-                {
-                    HPChosen = slimeHP[i];
-                    i = slimeHP.Count;
-                }
-            }
-            if (HPChosen == 0)
-            {
-                HPChosen = 1;
-            }
-            timer = 0.0f;
-
-           
-
-            GameObject objRef = Instantiate(slimePrefab[Random.Range(0, slimePrefab.Count)], new Vector3(Random.Range(-7f, 7f), Random.Range(3.3f, -3.3f), 0.0f), Quaternion.identity);
-            objRef.GetComponent<slime>().Init(HPChosen);
+            Debug.Log("EPIC WIN");
         }
+
+        //if (timer >= 2.0f && currentSlimes < maxSlimes)
+        //{
+        //    currentSlimes += 1;
+
+
+        //    float rand = Random.Range(0.0f, 100.0f);
+        //    int HPChosen = 0;
+
+        //    for (int i = 0; i < slimeHP.Count; i++)
+        //    {
+        //        float temp = persentgaes[i];
+
+        //        for (int j = 0; j < i; j++)
+        //        {
+        //            temp += persentgaes[j];
+        //        }
+        //        if (rand < temp)
+        //        {
+        //            HPChosen = slimeHP[i];
+        //            i = slimeHP.Count;
+        //        }
+        //    }
+        //    if (HPChosen == 0)
+        //    {
+        //        HPChosen = 1;
+        //    }
+        //    timer = 0.0f;
+
+
+
+        //    GameObject objRef = Instantiate(slimePrefab[Random.Range(0, slimePrefab.Count)], new Vector3(Random.Range(-7f, 7f), Random.Range(3.3f, -3.3f), 0.0f), Quaternion.identity);
+        //    objRef.GetComponent<slime>().Init(HPChosen);
+        //}
     }
 
     IEnumerator LAZERscan()
     {
-        for (int i = startXYpos.y; i < finishXYpos.y; i+= LAZERNODEdetail.y)
+        for (float i = startXYpos.y; i < finishXYpos.y; i+= LAZERNODEdetail.y)
         {
-            GameObject temp2 = Instantiate(LAZERNODEprefab, new Vector3(startXYpos.x, i, LAZERNODEheight), Quaternion.identity);
+            GameObject temp2 = Instantiate(LAZERNODEprefab, new Vector3(startXYpos.x, LAZERNODEheight, i), Quaternion.identity);
             temp2.AddComponent<LAZERNODE>();
+            temp2.GetComponent<LineRenderer>().startWidth = 0.05f;
+            temp2.GetComponent<LineRenderer>().endWidth = 0.05f;
+            temp2.GetComponent<LineRenderer>().enabled = false;
+
             temp2.transform.SetParent(this.gameObject.transform);
-        }
+            temp2.GetComponent<LAZERNODE>().startXYpos = startXYpos;
+            temp2.GetComponent<LAZERNODE>().finXYpos = finishXYpos;
+            temp2.GetComponent<LAZERNODE>().LAZERNODEdetail = LAZERNODEdetail;
 
-        for (int j = startXYpos.x; j < finishXYpos.x; j += LAZERNODEdetail.x)
-        {
-            for (int i = 0; i < transform.childCount; i++)
-            {
-                
-               transform.GetChild(i).GetComponent<LAZERNODE>().Scan();
-            }
-
-            this.gameObject.transform.localPosition += new Vector3(LAZERNODEdetail.x, 0.0f, 0.0f);
         }
 
 
-        int temp = transform.childCount;
-        for (int i = 0; i < temp; i++)
-        {
-            transform.GetChild(i).transform.gameObject.SetActive(false);        
-        }
+        for (int i = 0; i < transform.childCount; i++)
+         {
+            transform.GetChild(i).GetComponent<LAZERNODE>().Scan();
+         }
+
 
         yield return null;
 
