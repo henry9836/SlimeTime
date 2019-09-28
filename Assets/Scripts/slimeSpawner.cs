df41e5f4bf030fdd9d2267488dfec5d739d73cf2
Vaughan Webb <vaughanw2012@gmail.com>
2019-09-28 21:04:29 +1200

    quick touchup

--- Assets/Scripts/slimeSpawner.cs
+++ Assets/Scripts/slimeSpawner.cs
@@ -36,50 +36,37 @@ public class slimeSpawner : MonoBehaviour
             StartCoroutine(LAZERscan());
         }
 
-        
-
         int pointcount = Mathf.FloorToInt((finishXYpos.y - startXYpos.y) / LAZERNODEdetail.y) * Mathf.FloorToInt((finishXYpos.x - startXYpos.x) / LAZERNODEdetail.x);
-
-
 
         if (ValidPositionsFIRE.Count + ValidPositionsICE.Count + ValidPositionsNORMAL.Count + InvalidPositions.Count == pointcount)
         {
-            Debug.Log("EPIC WIN");
+            //if (timer >= 2.0f && currentSlimes < maxSlimes)
+            //{
+            //    currentSlimes += 1;
+            //    float rand = Random.Range(0.0f, 100.0f);
+            //    int HPChosen = 0;
+            //    for (int i = 0; i < slimeHP.Count; i++)
+            //    {
+            //        float temp = persentgaes[i];
+            //        for (int j = 0; j < i; j++)
+            //        {
+            //            temp += persentgaes[j];
+            //        }
+            //        if (rand < temp)
+            //        {
+            //            HPChosen = slimeHP[i];
+            //            i = slimeHP.Count;
+            //        }
+            //    }
+            //    if (HPChosen == 0)
+            //    {
+            //        HPChosen = 1;
+            //    }
+            //    timer = 0.0f;
+            //    GameObject objRef = Instantiate(slimePrefab[Random.Range(0, slimePrefab.Count)], new Vector3(Random.Range(-7f, 7f), Random.Range(3.3f, -3.3f), 0.0f), Quaternion.identity);
+            //    objRef.GetComponent<slime>().Init(HPChosen);
+            //}
         }
-
-        //if (timer >= 2.0f && currentSlimes < maxSlimes)
-        //{
-        //    currentSlimes += 1;
-
-
-        //    float rand = Random.Range(0.0f, 100.0f);
-        //    int HPChosen = 0;
-
-        //    for (int i = 0; i < slimeHP.Count; i++)
-        //    {
-        //        float temp = persentgaes[i];
-
-        //        for (int j = 0; j < i; j++)
-        //        {
-        //            temp += persentgaes[j];
-        //        }
-        //        if (rand < temp)
-        //        {
-        //            HPChosen = slimeHP[i];
-        //            i = slimeHP.Count;
-        //        }
-        //    }
-        //    if (HPChosen == 0)
-        //    {
-        //        HPChosen = 1;
-        //    }
-        //    timer = 0.0f;
-
-
-
-        //    GameObject objRef = Instantiate(slimePrefab[Random.Range(0, slimePrefab.Count)], new Vector3(Random.Range(-7f, 7f), Random.Range(3.3f, -3.3f), 0.0f), Quaternion.identity);
-        //    objRef.GetComponent<slime>().Init(HPChosen);
-        //}
     }
 
     IEnumerator LAZERscan()
