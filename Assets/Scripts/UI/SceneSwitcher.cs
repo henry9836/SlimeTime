4ac9e57250d50c3b045c3ab102681e25e8cf12dd
Pixelgrapher <pixelgrapher@outlook.co.nz>
2019-10-02 14:18:02 +1300

    Added UI scripts

---
+++ Assets/Scripts/UI/SceneSwitcher.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SceneSwitcher : MonoBehaviour {
+
+    public GameObject fadePanel;
+    public float fadeTime;
+    private Image fadeImage;
+    public string targetScene;
+    public string curScene;
+    public bool isFading;
+    public bool isSwitching;
+    public bool isRespawning;
+    public float fadeTimeCur;
+
+    void Awake()
+    {
+        //fadePanel = GameObject.Find("fadePanel");
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        curScene = SceneManager.GetActiveScene().name;
+        Debug.Log(curScene);
+        if (curScene == "MainMenu")
+        {
+            //GlobalData.LastScene = curScene;
+
+        }
+
+        
+
+        isFading = true;
+        fadeTimeCur = fadeTime;
+        fadePanel.SetActive(true);
+        fadeImage = fadePanel.GetComponent<Image>();
+        Vector4 initialColor = fadeImage.color;
+        fadeImage.DOFade(0, fadeTime).SetEase(Ease.InOutSine);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        fadeTimeCur = Mathf.MoveTowards(fadeTimeCur, 0f, Time.unscaledDeltaTime);
+        if (fadeTimeCur != 0)
+        {
+            isFading = true;
+        }
+        else
+        {
+            isFading = false;
+        }
+
+        if (isSwitching && !isFading)
+        {
+            if (targetScene == "")
+            {
+                ExitFade();
+            }
+            else
+            {
+                SceneManager.LoadScene(targetScene);
+            }
+        }
+
+    }
+
+    public void SceneSwitch(string scene)
+    {
+        if (!isSwitching)
+        {
+            Vector4 initialColor = fadeImage.color;
+            fadeImage.DOFade(1, fadeTime / 1.5f).SetEase(Ease.InOutSine);
+            fadeTimeCur = fadeTime;
+            targetScene = scene;
+            isSwitching = true;
+        }
+    }
+
+
+    public void StartFade()
+    {
+        if (!isSwitching)
+        {
+            Vector4 initialColor = fadeImage.color;
+            fadeImage.DOFade(1, fadeTime / 1.5f).SetEase(Ease.InOutSine);
+        }
+        isSwitching = true;
+        fadeTimeCur = fadeTime;
+    }
+
+    public void ExitFade()
+    {
+        isFading = true;
+        isSwitching = false;
+        fadeTimeCur = fadeTime;
+        Vector4 initialColor = fadeImage.color;
+        fadeImage.DOFade(0, fadeTime).SetEase(Ease.InOutSine);
+    }
+
+    public void EnterMainMenu()
+    {
+        StartFade();
+        targetScene = "MainMenu";
+    }
+}
---
+++ Assets/Scripts/UI/Wobble.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wobble : MonoBehaviour
+{
+    public float verticalWavePeriod;
+    public float verticalMagnitude;
+    public float rotateWavePeriod;
+    public float rotateMagnitude;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
