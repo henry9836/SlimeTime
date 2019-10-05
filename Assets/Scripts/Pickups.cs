3f53ef8e93174a7b1a0d999ede86c00618a2571d
Vaughan Webb <vaughanw2012@gmail.com>
2019-10-05 22:31:11 +1300

    initial

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickups : MonoBehaviour
+{
+
+    public enum POWERUPS
+    {
+        test,
+        test2, 
+        pizzatime,
+
+    }
+
+
+    public void pew(POWERUPS temp)
+    {
+        if (temp == POWERUPS.test)
+        {
+
+        }
+    }
+}
