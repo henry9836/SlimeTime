fd1d16e99e2c4aad448dfe9388b5d7f065871f82
Henry <henry983615@gmail.com>
2019-10-08 22:00:03 +1300

    Pickups do things
    We have rapid fire and dash now

---
+++ Assets/Scripts/PewPlayerMechanic.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PewPlayerMechanic : MonoBehaviour
+{
+
+    private GameObject playerRef;
+    private float burstCoolDownTime = 0.1f;
+
+    private bool canFire = true;
+
+    public void pew(Pickups.POWERUPS powerup, GameObject _playerRef)
+    {
+
+        playerRef = _playerRef;
+
+        Debug.Log("PEW");
+
+        if (powerup == Pickups.POWERUPS.DASH)
+        {
+            playerRef.GetComponent<Rigidbody>().AddForce(playerRef.transform.forward * 1000);
+            playerRef.GetComponent<PlayerController>().pickupAmmoCount--;
+        }
+        else if (powerup == Pickups.POWERUPS.SPRAY)
+        {
+            if (canFire)
+            {
+                Debug.Log("PEW2");
+                GameObject refer = Instantiate(playerRef.GetComponent<PlayerController>().baseProjectile, transform.position, Quaternion.identity);
+                refer.GetComponent<Rigidbody>().AddForce(playerRef.GetComponent<PlayerController>().lastAimVec * playerRef.GetComponent<PlayerController>().fireForce);
+                refer.transform.LookAt(transform.position + (playerRef.GetComponent<PlayerController>().lastAimVec * 100.0f));
+                refer.GetComponent<projectileController>().travelDir = playerRef.GetComponent<PlayerController>().lastAimVec;
+                playerRef.GetComponent<PlayerController>().pickupAmmoCount--;
+
+                StartCoroutine(burstCoolDown());
+            }
+        }
+    }
+
+    IEnumerator burstCoolDown()
+    {
+        canFire = false;
+        yield return new WaitForSeconds(burstCoolDownTime);
+        canFire = true;
+    }
+
+}
--- Assets/Scripts/pickupcontroler.cs
+++ Assets/Scripts/pickupcontroler.cs
@@ -10,11 +10,31 @@ public class pickupcontroler : MonoBehaviour
     public Pickups.POWERUPS type;
     public float amplitudeRate;
     private float landingypos;
-
+    private int ammoCount = 3;
 
     void Start()
     {
         lifetime = lifetimeStart;
+
+        //Set ammo counts
+        if (type == Pickups.POWERUPS.NULL)
+        {
+            Debug.LogWarning("Pickup has type NULL assigned: " + name);
+        }
+        else if (type == Pickups.POWERUPS.DASH)
+        {
+            ammoCount = 3;
+        }
+        else if (type == Pickups.POWERUPS.SPRAY)
+        {
+            ammoCount = 100;
+        }
+        else
+        {
+            Debug.LogWarning("Pickup has an unknown type assigned: " + type + " Object: " + name);
+        }
+
+
     }
     void FixedUpdate()
     {
@@ -34,6 +54,7 @@ public class pickupcontroler : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.GetComponent<PlayerController>().powerupType = type;
+            collision.gameObject.GetComponent<PlayerController>().pickupAmmoCount = ammoCount;
             StartCoroutine(despawn());
 
         }
@@ -51,7 +72,6 @@ public class pickupcontroler : MonoBehaviour
 
     IEnumerator despawn()
     {
-
         Destroy(this.gameObject);
         yield return null;
     }
