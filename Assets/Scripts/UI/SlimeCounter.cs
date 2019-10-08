41d5fe837648f8e93c6e12d04dde1ff99ec76dc0
Pixelgrapher <pixelgrapher@outlook.co.nz>
2019-10-09 11:19:43 +1300

    Start of slime counter implemented

---
+++ Assets/Scripts/UI/SlimeCounter.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class SlimeCounter : MonoBehaviour
+{
+
+    public GameObject mainPanel;
+    public Text countText;
+    public GameObject gameManagerObject;
+    private GameManager gameManager;
+
+    private bool isNull = true;
+
+    void Start()
+    {
+        if (gameManagerObject != null)
+        {
+            isNull = false;
+
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+        }
+    }
+
+    void Update()
+    {
+        if (!isNull)
+        {
+            countText.text = gameManager.remainingSpawn.ToString();
+        }
+        else
+        {
+            Debug.Log("Game Manager not found. Cannot display slime count.");
+            countText.text = "00";
+        }
+    }
+}
