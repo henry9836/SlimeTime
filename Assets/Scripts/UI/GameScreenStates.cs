e249b24cd3eea6defad82c074f3d2007a56e46d4
Pixelgrapher <pixelgrapher@outlook.co.nz>
2019-10-09 18:02:11 +1300

    Start of game screens implemented. WIP.

---
+++ Assets/Scripts/UI/GameScreenStates.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class GameScreenStates : MonoBehaviour
+{
+    public int screenState;
+
+    public GameObject screensPlay;
+    public GameObject screensGrace;
+    public GameObject screensWave;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
