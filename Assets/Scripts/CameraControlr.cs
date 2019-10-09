a0483e0628161600334202c34d6ffdf6801c6481
Vaugahan Webb <vaughanw2012@gmail.com>
2019-10-09 14:37:05 +1300

    WIP

--- Assets/Scripts/CameraControlr.cs
+++ Assets/Scripts/CameraControlr.cs
@@ -10,25 +10,127 @@ public class CameraControlr : MonoBehaviour
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
 
-        RaycastHit straightHIT;
-        Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out straightHIT, Mathf.Infinity);
-        Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * straightHIT.distance, Color.red);
 
 
-        RaycastHit player1HIT;
-        Physics.Linecast(transform.position, players[0].GetComponent<Renderer>().bounds.center, out player1HIT);
-        Debug.DrawLine(transform.position, players[0].GetComponent<Renderer>().bounds.center, Color.red);
+        Vector3 playersCOM = COM(players);
 
-        RaycastHit player1HITTOStraight;
-        Physics.Linecast(straightHIT.point, players[0].GetComponent<Renderer>().bounds.center, out player1HITTOStraight);
-        Debug.DrawLine(straightHIT.point, players[0].GetComponent<Renderer>().bounds.center, Color.red);
+        transform.position = new Vector3(playersCOM.x, 20.0f, playersCOM.z - 25.0f);
 
-        float z = straightHIT.distance;
-        float y = player1HIT.distance;
-        float x = player1HITTOStraight.distance;
+        float camFOV = FOV(players);
 
-        float angle = (Mathf.Acos((Mathf.Pow(z, 2) + Mathf.Pow(y, 2) - Mathf.Pow(x, 2)) / (2 * z * y)) * 180) / 3.1415926f;
+        gameObject.GetComponent<Camera>().orthographicSize = camFOV;
 
-        Debug.Log(angle);
+       
+
     }
+
+    Vector3 COM(GameObject[] players)
+    {
+        float xpos = 0.0f;
+        float ypos = 0.0f;
+        float zpos = 0.0f;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            xpos += players[i].transform.position.x;
+        }
+        xpos /= players.Length;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            ypos += players[i].transform.position.y;
+        }
+        ypos /= players.Length;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            zpos += players[i].transform.position.z;
+        }
+        zpos /= players.Length;
+
+        return (new Vector3(xpos, ypos, zpos));
+    }
+
+    float FOV(GameObject[] players)
+    {
+        float xdiff = gameObject.GetComponent<Camera>().orthographicSize * (16.0f / 9.0f);
+        float ydiff = (gameObject.GetComponent<Camera>().orthographicSize) * Mathf.Cos(gameObject.transform.rotation.x);
+        float zdiff = (gameObject.GetComponent<Camera>().orthographicSize) * Mathf.Sin(gameObject.transform.rotation.x);
+
+        RaycastHit straightHIT;
+        Physics.Raycast(gameObject.transform.localPosition, transform.TransformDirection(Vector3.forward), out straightHIT, Mathf.Infinity);
+        Debug.DrawRay(gameObject.transform.position, transform.TransformDirection(Vector3.forward) * straightHIT.distance, Color.white);
+
+        float shortestpos = playertoposition(transform.position, players);
+
+        /// code for raycasting off FOV lines 
+
+        RaycastHit straightHITtr;
+        Vector3 position = new Vector3(transform.localPosition.x + xdiff, transform.localPosition.y + ydiff, transform.localPosition.z + zdiff);
+        Physics.Raycast(position, transform.TransformDirection(Vector3.forward), out straightHITtr, Mathf.Infinity);
+        Debug.DrawRay(position, transform.TransformDirection(Vector3.forward) * straightHITtr.distance, Color.red);
+
+
+        RaycastHit straightHITtl;
+        position = new Vector3(transform.localPosition.x - xdiff, transform.localPosition.y + ydiff, transform.localPosition.z + zdiff);
+        Physics.Raycast(position, transform.TransformDirection(Vector3.forward), out straightHITtl, Mathf.Infinity);
+        Debug.DrawRay(position, transform.TransformDirection(Vector3.forward) * straightHITtl.distance, Color.green);
+
+
+
+        RaycastHit straightHITbr;
+        position = new Vector3(transform.localPosition.x + xdiff, transform.localPosition.y - ydiff, transform.localPosition.z - zdiff);
+        Physics.Raycast(position, transform.TransformDirection(Vector3.forward), out straightHITbr, Mathf.Infinity);
+        Debug.DrawRay(position, transform.TransformDirection(Vector3.forward) * straightHITbr.distance, Color.blue);
+
+
+
+        RaycastHit straightHITbl;
+        position = new Vector3(transform.localPosition.x - xdiff, transform.localPosition.y - ydiff, transform.localPosition.z - zdiff);
+        Physics.Raycast(position, transform.TransformDirection(Vector3.forward), out straightHITbl, Mathf.Infinity);
+        Debug.DrawRay(position, transform.TransformDirection(Vector3.forward) * straightHITbl.distance, Color.yellow);
+
+
+
+
+
+
+
+
+
+        return (7.0f);
+    }
+
+    public Vector3 FindNearestPointOnLine(Vector3 origin, Vector3 direction, Vector3 point)
+    {
+        direction.Normalize();
+        Vector3 lhs = point - origin;
+        float dotP = Vector3.Dot(lhs, direction);
+        return (origin + direction * dotP);
+    }
+
+    public float playertoposition(Vector3 lineposition, GameObject[] players)
+    {
+        float Xlongest = Mathf.Infinity;
+        float Ylongest = Mathf.Infinity;
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            Vector3 temp = FindNearestPointOnLine(lineposition, transform.TransformDirection(Vector3.forward), players[i].transform.position);
+            Debug.DrawLine(temp, players[i].transform.position);
+
+            //if ()
+            //if (Vector3.Distance(temp, players[i].transform.position) > shortdistance)
+            //{
+            //    shortdistance = Vector3.Distance(temp, players[i].transform.position);
+            //}
+        }
+
+
+        return (0.0f);
+    }
+
+
+
+
 }
