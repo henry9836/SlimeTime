ad9eff4b7a8ff5784e73571bc96f43528678c2c9
David Morris <pixelgrapher@outlook.co.nz>
2019-10-10 08:04:23 +1300

    Development on screen states

--- Assets/Scripts/UI/GameScreenStates.cs
+++ Assets/Scripts/UI/GameScreenStates.cs
@@ -6,19 +6,29 @@ using DG.Tweening;
 
 public class GameScreenStates : MonoBehaviour
 {
-    public int screenState;
+    public int screenState = -1;
+    private int screenStateCur = -1;
 
     public GameObject screensPlay;
     public GameObject screensGrace;
     public GameObject screensWave;
 
+    public GameObject[] screens;
+
     void Start()
     {
-        
+        screenState = 0;
+        screenStateCur = 0;
     }
 
     void Update()
     {
-        
+        if (screenState != screenStateCur)
+        {
+            screens[screenStateCur].SetActive(false);
+            screens[screenState].SetActive(true);
+
+            screenStateCur = screenState;
+        }
     }
 }
