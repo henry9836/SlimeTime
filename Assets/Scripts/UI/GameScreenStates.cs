df392d784238977e761d1843857943f6bc2fdf68
David Morris <pixelgrapher@outlook.co.nz>
2019-10-13 11:37:45 +1300

    Game Screens UI now functional.
    UI animations still WIP.

--- Assets/Scripts/UI/GameScreenStates.cs
+++ Assets/Scripts/UI/GameScreenStates.cs
@@ -6,13 +6,33 @@ using DG.Tweening;
 
 public class GameScreenStates : MonoBehaviour
 {
+
+    private GameObject gameManagerObject;
+    private GameManager gameManager;
+    private bool isNull = true;
+
     public int screenState = -1;
     private int screenStateCur = -1;
+    // 0 - Normal gameplay with slime counter
+    // 1 - Wave end text
+    // 2 - Next wave countdown
+    // 3 - Wave start text
 
     public GameObject[] screens;
 
     void Start()
     {
+        if (gameManagerObject == null)
+        {
+            gameManagerObject = GameObject.Find("GameManager");
+        }
+
+        if (gameManagerObject != null)
+        {
+            isNull = false;
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+        }
+
         screenState = 0;
         screenStateCur = 0;
     }
@@ -21,8 +41,9 @@ public class GameScreenStates : MonoBehaviour
     {
         if (screenState != screenStateCur)
         {
+            // Deactivate old screen
             if (screenStateCur >= 0)
-            { // Do not allow array index out of range. Deactivate old screen
+            {
                 screens[screenStateCur].SetActive(false);
             }
 
@@ -30,6 +51,23 @@ public class GameScreenStates : MonoBehaviour
             screens[screenState].SetActive(true);
 
             screenStateCur = screenState;
+        }
+        else
+        {
+            if (screenState == 0 && gameManager.remainingSpawn <= 0)
+            {
+                screenState = 1;
+            }
+
+            if (screenState == 1 && gameManager.gracetimer <= 5.0f)
+            {
+                screenState = 2;
+            }
+
+            if (screenState == 2 && gameManager.gracetimer <= 0.1f)
+            {
+                screenState = 0;
+            }
         }
     }
 }
---
+++ Assets/Scripts/UI/WaveCountdown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class WaveCountdown : MonoBehaviour
+{
+    private GameObject gameManagerObject;
+    private GameManager gameManager;
+    private bool isNull = true;
+
+    private float startTimerValue = 5.0f;
+    private GameObject countdownTimer;
+    private Image countdownFill;
+
+    void Awake()
+    {
+        if (gameManagerObject == null)
+        {
+            gameManagerObject = GameObject.Find("GameManager");
+        }
+
+        if (gameManagerObject != null)
+        {
+            isNull = false;
+            gameManager = gameManagerObject.GetComponent<GameManager>();
+        }
+
+        countdownTimer = transform.Find("graceTime").gameObject;
+        countdownFill = transform.Find("timerImage").GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        countdownTimer.GetComponent<Text>().text = Mathf.Ceil(gameManager.gracetimer).ToString();
+        countdownFill.fillAmount = gameManager.gracetimer / startTimerValue;
+    }
+}
