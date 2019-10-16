db63028c5e21b27759e333e9285c3fc874eb0eb8
Pixelgrapher <pixelgrapher@outlook.co.nz>
2019-10-16 17:19:53 +1300

    Start of HP bars

---
+++ Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public GameObject mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(mainCamera.transform.position);
+    }
+}
