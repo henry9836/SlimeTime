c3f240a7941fc66e902cd24fd31acd51449d751a
Vaugahan Webb <vaughanw2012@gmail.com>
2019-10-16 17:45:08 +1300

    script creation

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HealthBar : MonoBehaviour
 {
     public GameObject mainCamera;
+    public GameObject bar;
+    public List<GameObject> barsList; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,37 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(mainCamera.transform.position);
+
+        GameObject[] validPlayers = GameObject.FindGameObjectsWithTag("Player");
+        List<GameObject> Players = new List<GameObject>();
+        for (int i = 0; i < validPlayers.Length; i++)
+        {
+            if (validPlayers[i].GetComponent<PlayerController>().health > 0)
+            {
+                Players.Add(validPlayers[i]);
+                barsList.Add(bar);
+            }
+        }
+
+        for (int i = 0; i < barsList.Count; i++)
+        {
+            gameObject.transform.GetChild(7).GetChild(i).gameObject.SetActive(true);
+        }
+
+
+        for (int i = 0; i < Players.Count; i++)
+        {
+            int layerMask = 1 << 12;
+            RaycastHit playerpos;
+            Physics.Raycast(Players[i].gameObject.transform.position, GameObject.Find("Main Camera").transform.TransformDirection(Vector3.forward), out playerpos, Mathf.Infinity, layerMask);
+            Debug.DrawLine(Players[i].transform.position, playerpos.point, Color.cyan);
+
+            Vector3 dir = new Vector3(playerpos.point.x, playerpos.point.z, 0.0f);
+
+            barsList[i].GetComponent<RectTransform>().localPosition = dir;
+        }
+
+        //transform.LookAt(mainCamera.transform.position);
+
     }
 }
